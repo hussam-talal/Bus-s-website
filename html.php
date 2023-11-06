36ba91520fef31c523af4cd1aafc67f97312fa91
husam-talal <hhhusam777056229@gmail.com>
2023-11-06 16:48:04 +0300

    hussamsoft commit

--- html.php
+++ html.php
@@ -11,8 +11,8 @@
         <meta property="og:url" content="https://ksavipbus.com/"/>
             
 		
-        <link rel="alternate" href="https://ksavipbus.com/en/home/" hreflang="en"/>
-        <link rel="alternate" href="https://ksavipbus.com/" hreflang="ar"/>
+        <link rel="alternate" href="https://bus.com/en/home/" hreflang="en"/>
+        <link rel="alternate" href="https://bus.com/" hreflang="ar"/>
         <link rel="alternate" type="application/rss+xml" title="Ksa Vip Bus &laquo; الخلاصة" href="https://ksavipbus.com/feed/"/>
         <link rel="alternate" type="application/rss+xml" title="Ksa Vip Bus &laquo; خلاصة التعليقات" href="https://ksavipbus.com/comments/feed/"/>
         <!-- This site uses the Google Analytics by MonsterInsights plugin v8.20.1 - Using Analytics tracking - https://www.monsterinsights.com/ -->
@@ -950,7 +950,7 @@
                                                                 
                                                                 <ul id="menu-header-list-ar" class="jkit-menu jkit-menu-direction-flex jkit-submenu-position-top">
                                                                     <li id="menu-item-1149" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-1074 current_page_item menu-item-1149">
-                                                                        <a href="https://ksavipbus.com/" aria-current="page">الرئيسية</a>
+                                                                        <a href="" aria-current="page">الرئيسية</a>
                                                                     </li>
                                                                     <li id="menu-item-784" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-784">
                                                                         <a href="">الشركات</a>
@@ -985,13 +985,13 @@
                                                                         </a>
                                                                         <ul class="sub-menu">
                                                                             <li id="menu-item-752-en" class="lang-item lang-item-6 lang-item-en lang-item-first menu-item menu-item-type-custom menu-item-object-custom menu-item-752-en">
-                                                                                <a href="https://ksavipbus.com/en/home/" hreflang="en-US" lang="en-US">
+                                                                                <a href="" hreflang="en-US" lang="en-US">
                                                                                     <img decoding="async" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAALCAMAAABBPP0LAAAAmVBMVEViZsViZMJiYrf9gnL8eWrlYkjgYkjZYkj8/PujwPybvPz4+PetraBEgfo+fvo3efkydfkqcvj8Y2T8UlL8Q0P8MzP9k4Hz8/Lu7u4DdPj9/VrKysI9fPoDc/EAZ7z7IiLHYkjp6ekCcOTk5OIASbfY/v21takAJrT5Dg6sYkjc3Nn94t2RkYD+y8KeYkjs/v7l5fz0dF22YkjWvcOLAAAAgElEQVR4AR2KNULFQBgGZ5J13KGGKvc/Cw1uPe62eb9+Jr1EUBFHSgxxjP2Eca6AfUSfVlUfBvm1Ui1bqafctqMndNkXpb01h5TLx4b6TIXgwOCHfjv+/Pz+5vPRw7txGWT2h6yO0/GaYltIp5PT1dEpLNPL/SdWjYjAAZtvRPgHJX4Xio+DSrkAAAAASUVORK5CYII=" alt="English" width="16" height="11" style="width: 16px; height: 11px;"/>
                                                                                     <span style="margin-right:0.3em;">English</span>
                                                                                 </a>
                                                                             </li>
                                                                             <li id="menu-item-752-ar" class="lang-item lang-item-9 lang-item-ar current-lang menu-item menu-item-type-custom menu-item-object-custom current_page_item menu-item-home menu-item-752-ar">
-                                                                                <a href="https://ksavipbus.com/" hreflang="ar" lang="ar">
+                                                                                <a href="" hreflang="ar" lang="ar">
                                                                                     <img decoding="async" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAALCAMAAABBPP0LAAAANlBMVEUAYjMTYDs3R0AvV0NObzE3dSoTWzhAZjgyfEY0gl1EcDFqpIhKj28TVzaLs41ol1JSaF1JW1NzUHm9AAAAPUlEQVR4AY2MtQEAMAgE447tv2xKvuQqeEtRcikZ/9p6b9X/Mdfeaw4PnPvehQhNvpcnJYiInIqraqYpyAd1AAFxIEreLQAAAABJRU5ErkJggg==" alt="العربية" width="16" height="11" style="width: 16px; height: 11px;"/>
                                                                                     <span style="margin-right:0.3em;">العربية</span>
                                                                                 </a>
